22fa85c50a2fbbd83578131087286490c23c6b6d
Luis Paulo <luispaulo.ads8@gmail.com>
2023-02-27 23:14:57 -0300

    Atualizacao  RepositorioAluno

--- EM.Repository/RepositorioAluno.cs
+++ EM.Repository/RepositorioAluno.cs
@@ -2,36 +2,29 @@
 using FirebirdSql.Data.FirebirdClient;
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
+using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace EM.Repository
 {
     public class RepositorioAluno : RepositorioAbstrato<Aluno>
-    {
+    { 
+
         public override void Add(Aluno obj)
         {
             ConexaoFirebird.Conectar();
-            using (FbConnection conexao = ConexaoFirebird.Conexao)
-            {
-                try
-                {
-                    string sql = "INSERT into TBALUNO Values(" + obj.Matricula + ",'" + obj.Nome + "','" + obj.CPF + "','" + obj.Nascimento.ToString("yyyy-MM-dd") + "','" + ((int)obj.Sexo) + "')";
-                    FbCommand cmd = new FbCommand(sql, conexao);
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception)
-                {
+            using (FbConnection conexao = ConexaoFirebird.Conexao){
 
-                    throw new Exception("Não Conectado!");
-                }
-                finally
-                {
-                    ConexaoFirebird.Desconectar();
-                }
+                const string sql = "INSERT into TBALUNO (MATRICULA, NOME, CPF, NASCIMENTO, SEXO) Values(@MATRICULA, @NOME, @CPF, @NASCIMENTO, @SEXO)";
+                FbCommand cmd = new FbCommand(sql, conexao);
+                cmd.Parameters.Add("@MATRICULA", obj.Matricula);
+                cmd.Parameters.Add("@NOME", obj.Nome);
+                cmd.Parameters.Add("@CPF", obj.CPF);
+                cmd.Parameters.Add("@NASCIMENTO", obj.Nascimento);
+                cmd.Parameters.Add("@SEXO", obj.Sexo);
+                cmd.ExecuteNonQuery();
             }
         }
 
@@ -40,16 +33,16 @@ namespace EM.Repository
             return Get(A => A.Matricula.Equals(predicate));
         }
 
+
         public override IEnumerable<Aluno> GetAll()
         {
             ConexaoFirebird.Conectar();
             using (FbConnection conexao = ConexaoFirebird.Conexao)
             {
 
-                try
-                {
+            
                     List<Aluno> alunos = new List<Aluno>();
-                    string sql = "Select * from TBALUNO order by nome";
+                    string sql = "SELECT * FROM TBALUNO ORDER BY NOME";
                     FbCommand data = new FbCommand(sql, conexao);
                     FbDataReader dr = data.ExecuteReader();
 
@@ -59,26 +52,18 @@ namespace EM.Repository
                     {
                         aluno = new Aluno()
                         {
-                            Matricula = Convert.ToInt32(dr[0]),
-                            Nome = dr[1].ToString(),
-                            CPF = dr[2].ToString(),
-                            Nascimento = Convert.ToDateTime(dr[3]),
-                            Sexo = (EnumeradorSexo)Enum.ToObject(typeof(EnumeradorSexo), dr[4])
+                            Matricula = Convert.ToInt32(dr["MATRICULA"]),
+                            Nome = dr["NOME"].ToString(),
+                            CPF = dr["CPF"].ToString(),
+                            Nascimento = Convert.ToDateTime(dr["NASCIMENTO"]),
+                            Sexo = (EnumeradorSexo)Enum.ToObject(typeof(EnumeradorSexo), dr["SEXO"])
                         };
                         alunos.Add(aluno);
                     }
 
-                    return alunos;
-                }
-                catch (Exception)
-                {
-                    throw new Exception("Não Conectado!");
-                }
-                finally
-                {
-                    ConexaoFirebird.Desconectar();
-                }
+                return alunos;
             }
+
         }
 
         public override void Remove(Aluno obj)
@@ -86,22 +71,16 @@ namespace EM.Repository
             ConexaoFirebird.Conectar();
             using (FbConnection conexao = ConexaoFirebird.Conexao)
             {
-                try
-                {
-                    string sql = "DELETE from TBALUNO where matricula = " + obj.Matricula;
-                    //const string sql = "DELETE FROM TBALUNO WHERE MATRICULA = @MATRICULA AND NOME = @NOME AND CPF = @CPF AND NASCIMENTO = @NASCIMENTO AND SEXO = @ SEXO";
-                    DbCommand cmd = conexao.CreateCommand();
-                    FbCommand data = new FbCommand(sql, conexao);
-                    data.ExecuteNonQuery();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-                finally
-                {
-                    ConexaoFirebird.Desconectar();
-                }
+              
+                    const string sql = "DELETE FROM TBALUNO WHERE MATRICULA = @MATRICULA AND NOME = @NOME AND CPF = @CPF AND NASCIMENTO = @NASCIMENTO AND SEXO = @SEXO";
+                    FbCommand cmd = new FbCommand(sql, conexao);
+                    cmd.Parameters.Add("@MATRICULA", obj.Matricula);
+                    cmd.Parameters.Add("@NOME", obj.Nome);
+                    cmd.Parameters.Add("@CPF", obj.CPF);
+                    cmd.Parameters.Add("@NASCIMENTO", obj.Nascimento);
+                    cmd.Parameters.Add("@SEXO", obj.Sexo);
+                    cmd.ExecuteNonQuery();
+            
             }
         }
 
@@ -110,21 +89,15 @@ namespace EM.Repository
             ConexaoFirebird.Conectar();
             using (FbConnection conexao = ConexaoFirebird.Conexao)
             {
-                try
-                {
-                    string sql = "Update TBALUNO set matricula= '" + obj.Matricula + "', nome=  '" + obj.Nome + "', CPF= '" + obj.CPF + "', nascimento='" + obj.Nascimento.ToString("yyyy-MM-dd") + "',sexo='" + ((int)obj.Sexo) + "'" + " where matricula= " + obj.Matricula;
-                    FbCommand cmd = new FbCommand(sql, conexao);
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception)
-                {
 
-                    throw new Exception("Não Conectado!");
-                }
-                finally
-                {
-                    ConexaoFirebird.Desconectar();
-                }
+                string sql = "UPDATE TBALUNO SET MATRICULA = @MATRICULA, NOME = @NOME, CPF = @CPF, NASCIMENTO = @NASCIMENTO, SEXO = @SEXO WHERE MATRICULA = @MATRICULA";
+                FbCommand cmd = new FbCommand(sql, conexao);
+                cmd.Parameters.AddWithValue("@MATRICULA", obj.Matricula);
+                cmd.Parameters.AddWithValue("@NOME", obj.Nome);
+                cmd.Parameters.AddWithValue("@CPF", obj.CPF);
+                cmd.Parameters.AddWithValue("@NASCIMENTO", obj.Nascimento);
+                cmd.Parameters.AddWithValue("@SEXO", obj.Sexo);
+                cmd.ExecuteNonQuery();
             }
         }
 
@@ -133,34 +106,25 @@ namespace EM.Repository
             ConexaoFirebird.Conectar();
             using (FbConnection conexao = ConexaoFirebird.Conexao)
             {
-                try
+
+                string sql = "SELECT * FROM TBALUNO WHERE MATRICULA = " + matricula;
+                FbCommand data = new FbCommand(sql, conexao);
+                
+                FbDataReader dr = data.ExecuteReader();
+
+                Aluno aluno = new Aluno();
+
+                while (dr.Read())
                 {
-                    string sql = "Select * from TBALUNO where Matricula = " + matricula;
-                    FbCommand data = new FbCommand(sql, conexao);
-                    FbDataReader dr = data.ExecuteReader();
-                    
-                    Aluno aluno = new Aluno();
-
-                    while (dr.Read())
-                    {
-                        aluno.Matricula = Convert.ToInt32(dr[0]);
-                        aluno.Nome = dr[1].ToString();
-                        aluno.CPF = dr[2].ToString();
-                        aluno.Nascimento = Convert.ToDateTime(dr[3].ToString());
-                        aluno.Sexo = (EnumeradorSexo)Enum.ToObject(typeof(EnumeradorSexo), dr[4]);
-
-                    }
-                    return aluno;
+                    aluno.Matricula = Convert.ToInt32(dr["MATRICULA"]);
+                    aluno.Nome = dr["NOME"].ToString();
+                    aluno.CPF = dr["CPF"].ToString();
+                    aluno.Nascimento = Convert.ToDateTime(dr["NASCIMENTO"].ToString());
+                    aluno.Sexo = (EnumeradorSexo)Enum.ToObject(typeof(EnumeradorSexo), dr["SEXO"]);
 
                 }
-                catch (Exception)
-                {
-                    throw new Exception("Não Conectado!");
-                }
-                finally
-                {
-                    ConexaoFirebird.Desconectar();
-                }
+                return aluno;
+
             }
         }
 
@@ -169,20 +133,9 @@ namespace EM.Repository
             ConexaoFirebird.Conectar();
             using (FbConnection conexao = ConexaoFirebird.Conexao)
             {
-                try
-                {
                     IEnumerable<Aluno> alunos = GetAll();
                     alunos = alunos.Where(x => x.Nome.ToUpper().Contains(partDoNome.ToUpper()));
                     return alunos;
-                }
-                catch (Exception)
-                {
-                    throw new Exception("Não Conectado!");
-                }
-                finally
-                {
-                    ConexaoFirebird.Desconectar();
-                }
             }
 
         }
